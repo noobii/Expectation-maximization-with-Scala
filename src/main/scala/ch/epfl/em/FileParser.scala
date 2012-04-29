0979f6bfd9e6135ef70210ec431f399f5a90986a
Pierre Grydbeck <pierre@grydbeck.com>
2012-04-29 16:13:08 +0200

    Cleaned up the File processing

--- src/main/scala/ch/epfl/em/FileParser.scala
+++ src/main/scala/ch/epfl/em/FileParser.scala
@@ -7,58 +7,38 @@ import scalala.operators.Implicits.richArray
 import scalala.tensor.dense.DenseMatrix
 import scalala.tensor.dense.DenseVector
 
-/**
- * WARING COPY / PASTE CODE !
- */
+
 object FileParser {
 
-  // TODO make code nicer, and not copy paste !
   def toVectorSeq(fileName: String): Array[DenseVector[Double]] = {
-    def lineToVector(line: String): DenseVector[Double] = line.split(',').map(_.toDouble).asVector.asRow
+    def lineToVector(line: String): DenseVector[Double] = line.split(',').map(_.toDouble).asVector.asRow 
 
-    val source = fromFile(fileName)
-    
-    val lines = source.getLines() toArray
-    
+    processFile(fileName, lineToVector(_)) toArray
 
-    val vects = lines map(lineToVector(_))
-            
-    source.close();
-    
-    vects    
   }  
   
   def toMatrix(fileName: String): DenseMatrix[Double] = {
     def lineToMatrix(line: String): DenseMatrix[Double] = line.split(',').map(_.toDouble).asMatrix(1)
 
-    val source = fromFile(fileName)
+    val lineMatrices = processFile(fileName, lineToMatrix(_))
     
-    val lines = source.getLines() toArray
-    
-
-    val vects = lines map(lineToMatrix(_))
-    
-    val matrix = vects.reduce(DenseMatrix.vertcat(_, _))
-        
-    source.close();
+    val matrix = lineMatrices reduce (DenseMatrix.vertcat(_, _))
     
     matrix
   }
 
-  def toVector(fileName: String): DenseVector[Int] = {
+  def toVector(fileName: String): DenseVector[Int] = processFile(fileName, _.toInt).toArray.asVector
+  
+  private def processFile[V](fileName: String, applyToLine: String => V) = {
     val source = fromFile(fileName)
     
-    val lines = source.getLines().map(_.toInt) toArray
-    val vector = lines asVector
+    val lines = source getLines() toArray
     
+    val maped = lines map(applyToLine(_))
+            
     source.close()
-    
-    vector
-  }
-  
-  private def processFile[V](fileName: String, applyToRow: Array[_] => V): Array[V] = {
-    
-    null
+        
+    maped
   }
   
 }

a86a3c91d9f255d6927bc2156cb45558232d6825
Pierre Grydbeck <pierre@grydbeck.com>
2012-05-01 11:13:23 +0200

    Bugs in script

--- bcScript.sh
+++ bcScript.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+git pull
+
 HOST=$(hostname)
 DATE=$(date +"%Y-%m-%d-%H_%M_%S")
 REV=$(git rev-parse)
 REVSHORT=$(git rev-parse --short)
 
+echo $REV
+echo $REVSHORT
+
 OUTPUT="log/$REVSHORT-$DATE-$HOST.txt"
-OUTPUT-LONG="log/$REVSHORT-$DATE-$HOST-long.txt"
+OUTPUTLONG="log/$REVSHORT-$DATE-$HOST-long.txt"
 
 echo "log file: $OUTPUT"
 
 touch $OUTPUT
-touch $OUTPUT-LONG
+touch $OUTPUTLONG
 
-git pull | tee -a $OUTPUT-LONG
-./sbt compile | tee $a $OUTPUT-LONG
+./sbt compile | tee $a $OUTPUTLONG
 
 ./sbt run | tee -a $OUTPUT-LONG $OUTPUT

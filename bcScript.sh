24c38b90d04d76bc7d62ced3529cdc8f753bc3e8
Pierre Grydbeck <pierre@grydbeck.com>
2012-05-01 11:47:23 +0200

    Fixed bugs in script

--- bcScript.sh
+++ bcScript.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Gets the latest version from git
 GITOUT=$(git pull)
 
 HOST=$(hostname)
@@ -7,19 +8,24 @@ DATE=$(date +"%Y-%m-%d-%H_%M_%S")
 REV=$(git rev-parse HEAD)
 REVSHORT=$(git rev-parse --short HEAD)
 
-echo $REV
-echo $REVSHORT
 
 OUTPUT="log/$REVSHORT-$DATE-$HOST.txt"
 OUTPUTLONG="log/$REVSHORT-$DATE-$HOST-long.txt"
 
 echo "log file: $OUTPUT"
 
+#Creates the output files
 touch $OUTPUT
 touch $OUTPUTLONG
 
-$GITOUT >> $OUTPUTLONG
+#Saves what was outputed from Git
+echo $GITOUT >> $OUTPUTLONG
 
-./sbt compile | tee $a $OUTPUTLONG
+echo "Commit: $REV" >> $OUTPUTLONG $OUTPUT
+
+#Saves who is on the computer
+who | tee -a $OUTPUTLONG $OUTPUT
+
+./sbt compile | tee -a $OUTPUTLONG
 
 ./sbt run | tee -a $OUTPUTLONG $OUTPUT

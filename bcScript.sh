e4200ed61a4b46d2dcf8a35ebaa4e70881f53ef1
Pierre Grydbeck <pierre@grydbeck.com>
2012-05-01 11:09:17 +0200

    Now the script is getting really nice..

--- bcScript.sh
+++ bcScript.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+HOST=$(hostname)
 DATE=$(date +"%Y-%m-%d-%H_%M_%S")
-echo "$DATE: Running script..."
-git pull
-./sbt compile
-./sbt run | tee "$DATE.txt"
+REV=$(git rev-parse)
+REVSHORT=$(git rev-parse --short)
+
+OUTPUT="log/$REVSHORT-$DATE-$HOST.txt"
+OUTPUT-LONG="log/$REVSHORT-$DATE-$HOST-long.txt"
+
+echo "log file: $OUTPUT"
+
+touch $OUTPUT
+touch $OUTPUT-LONG
+
+git pull | tee -a $OUTPUT-LONG
+./sbt compile | tee $a $OUTPUT-LONG
+
+./sbt run | tee -a $OUTPUT-LONG $OUTPUT

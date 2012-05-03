0c093a68e9674eb23e7c2b76e73388038b4f130e
Pierre Grydbeck <pierre@grydbeck.com>
2012-05-03 18:44:44 +0200

    Added class to init data from matlab. Not tested it yet

--- src/main/scala/ch/epfl/em/GaussianInit.scala
+++ src/main/scala/ch/epfl/em/GaussianInit.scala
@@ -1,5 +1,7 @@
 package ch.epfl.em
 
+import java.io.File
+
 import scalala.tensor.dense.DenseMatrix
 import scalala.tensor.dense.DenseVector
 
@@ -13,4 +15,21 @@ trait GaussianInit {
   def means: DenseMatrix[Double]
   def covariances: Array[DenseMatrix[Double]]
 
+}
+
+/**
+ * 
+ */
+class InitFromMatlab(folderPath: String) extends GaussianInit {
+  
+  def weights = new FileParser(folderPath + "kmeanW.csv").toVectorDouble
+  def means = new FileParser(folderPath + "kmeanM.csv").toMatrix
+  
+  def covariances = {
+    val files = new File(folderPath).listFiles
+    
+    val covFiles = files filter(_.getName().contains("kmeanV")) sortBy(_.getName())
+    
+    covFiles map(x => new FileParser(x.getAbsolutePath).toMatrix)
+  }
 }

bc0ba964ea3b7d5f762d0160ed53bc0f65f8809d
Pierre Grydbeck <pierre@grydbeck.com>
2012-05-04 10:32:11 +0200

    Reverted to last working version of the script

--- bcScript.sh
+++ bcScript.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-# A bit messy but it works
-
 #Gets the latest version from git
 GITOUT=$(git pull)
-#Gets the hostname of
+
 HOST=$(hostname)
-#Usable date
 DATE=$(date +"%Y-%m-%d-%H_%M_%S")
-#Revision number
 REV=$(git rev-parse HEAD)
-#Short revision number
 REVSHORT=$(git rev-parse --short HEAD)
 
-#Two output files, on for everything, one only with important info
+
 OUTPUT="log/$REVSHORT-$DATE-$HOST.txt"
 OUTPUTLONG="log/$REVSHORT-$DATE-$HOST-long.txt"
-e
+
 echo "log file: $OUTPUT"
 
 #Creates the output files
@@ -31,8 +26,6 @@ echo "Commit: $REV" >> $OUTPUTLONG $OUTPUT
 #Saves who is on the computer
 who | tee -a $OUTPUTLONG $OUTPUT
 
-#Compiles the code, shows what is going on and saves it to long output
 ./sbt compile | tee -a $OUTPUTLONG
 
-#Runs the code, shows what is going on and saves to both outputs
 ./sbt run | tee -a $OUTPUTLONG $OUTPUT

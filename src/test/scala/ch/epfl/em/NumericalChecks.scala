864d6c0d9a452d52064c8020bffb256f324cd05d
Pierre Grydbeck <pierre@grydbeck.com>
2012-05-19 14:52:02 +0200

    Added implicits for numerical checks.

--- src/test/scala/ch/epfl/em/NumericalChecks.scala
+++ src/test/scala/ch/epfl/em/NumericalChecks.scala
@@ -2,38 +2,49 @@ package ch.epfl.em
 import scalala.tensor.dense.DenseMatrix
 import scalala.tensor.dense.DenseVector
 import scala.math.abs
+import CheckMatrix.matrix2CheckMatrix
 
 object NumericalChecks {
 
   val delta = 0.01
   
-  // TODO Could use implicits !!! =)
-  
   def closeEnough(mat1: DenseMatrix[Double], mat2: DenseMatrix[Double]): Boolean = {
     
-    if(mat1.numCols == mat2.numCols && mat1.numRows == mat2.numRows) {
+    (mat1.numCols == mat2.numCols && mat1.numRows == mat2.numRows) && {
       
       val diff = mat1 - mat2
             
-      return diff.forallValues(abs(_) < delta)
+      diff.forallValues(abs(_) < delta)
       
     }
-    
-    false
+
   }
   
   def closeEnough(vect1: DenseVector[Double], vect2: DenseVector[Double]): Boolean = {
-    if(vect1.size == vect2.size) {
+    (vect1.size == vect2.size) && {
       val diff = vect1 - vect2
       
-      return diff.forallValues(abs(_) < delta)
+      diff.forallValues(abs(_) < delta)
     }
-    
-    false
   }
   
   def closeEnough(cov1: Array[DenseMatrix[Double]], cov2: Array[DenseMatrix[Double]]): Boolean = {
-    (cov1.length == cov2.length) && ((cov1 zip cov2) forall {case(mat1, mat2) => closeEnough(mat1, mat2)})
-
+    (cov1.length == cov2.length) && ((cov1 zip cov2) forall {case(mat1, mat2) => mat1 closeEnough mat2})
   }
+}
+
+class CheckMatrix(matrix: DenseMatrix[Double]) {  
+  def closeEnough(otherMatrix: DenseMatrix[Double]): Boolean = NumericalChecks.closeEnough(matrix, otherMatrix)
+}
+
+object CheckMatrix {
+  implicit def matrix2CheckMatrix(matrix: DenseMatrix[Double]): CheckMatrix = new CheckMatrix(matrix)
+}
+
+class CheckVector(vector: DenseVector[Double]) {
+  def closeEnough(otherVector: DenseVector[Double]): Boolean = NumericalChecks.closeEnough(vector, otherVector)
+}
+
+object CheckVector {
+  implicit def vector2CheckVector(vector: DenseVector[Double]): CheckVector = new CheckVector(vector)
 }

6267434702afd986ca759ac3c40bc9ebc11b34e1
Pierre Grydbeck <pierre@grydbeck.com>
2012-05-23 09:56:05 +0200

    Switched back to crunch to benchmark... this is exciting

--- src/main/scala/ch/epfl/em/GaussianMenthor.scala
+++ src/main/scala/ch/epfl/em/GaussianMenthor.scala
@@ -152,7 +152,7 @@ class GaussianMenthor(
 	  value.exp = exNorm
 	  
 	  List()
-    } crunchToOne((x, y) => 
+    } crunch((x, y) => 
          new VertexValue(exp = x.exp + y.exp)
          // The estimated weights are computed by summing up all expectations
     ) then {
@@ -166,7 +166,7 @@ class GaussianMenthor(
         }
       }
       List()
-    } crunchToOne((x, y) => 
+    } crunch((x, y) => 
          new VertexValue(estMeans = x.means + y.means)
          // The estimated means are computed by summing computed values at each vertex
     ) then {
@@ -187,7 +187,7 @@ class GaussianMenthor(
         }
       }
       List()
-    } crunchToOne((x, y) => {
+    } crunch((x, y) => {
       val covarianceSum = (x.covariances zip y.covariances) map{case(mat1, mat2) => mat1 + mat2}
       
       new VertexValue(estCovariances = covarianceSum)

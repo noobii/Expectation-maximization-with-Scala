e4be2d6493d536e7388280b51d67d21cb4ffa9fe
Pierre Grydbeck <pierre@grydbeck.com>
2012-05-26 23:56:55 +0200

    First verison of benchmark script for the mtcquad

---
+++ mtcquad.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#gets the latest version from git
+git pull
+
+#builds the latest version
+./sbt one-jar
+
+HOST=$(hostname)
+DATE=$(date +"%Y-%m-%d-%H_%M_%S")
+
+OUTPUT="benchmark/$DATE-$REVSHORT-$HOST.txt"
+
+#will now loop through all configs
+
+for i in {0..15}
+do
+  taskset -c 0-$i java -jar target/scala-2.9.2/em_2.9.2-0.1-SNAPSHOT-one-jar.jar
+done

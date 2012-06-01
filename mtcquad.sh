2b987a7cc441e8dc2393e6231e8266b45459dd8d
Pierre Grydbeck <pierre@grydbeck.com>
2012-06-01 08:56:30 +0200

    Changed the bash script

--- mtcquad.sh
+++ mtcquad.sh
@@ -11,7 +11,10 @@ zip -d target/scala-2.9.2/em_2.9.2-0.1-SNAPSHOT-one-jar.jar lib/scala-*
 HOST=$(hostname)
 DATE=$(date +"%Y-%m-%d-%H_%M_%S")
 
-OUTPUT="benchmark/$DATE-$REVSHORT-$HOST.txt"
+RES="benchmark/$DATE-$REVSHORT-$HOST/"
+mkdir $RES
+
+OUTPUT="$RES/log.txt"
 touch OUTPUT
 
 #will now loop through all configs
@@ -19,10 +22,18 @@ touch OUTPUT
 for i in {0..7}
 do
   echo "$i cores"
+  
+  mkdir "$RES/$i"
+  
+  
   if [ $# -eq 2 ]
   then
     taskset -c 0-$i java -jar target/scala-2.9.2/em_2.9.2-0.1-SNAPSHOT-one-jar.jar $1 $2 | tee -a $OUTPUT
   else
     taskset -c 0-$i java -jar target/scala-2.9.2/em_2.9.2-0.1-SNAPSHOT-one-jar.jar | tee -a $OUTPUT
   fi
+
+#move the benchmark results
+mv benchmark/*.txt "$RES/$i"
+
 done

fa678aefde258fdbd4a2093a5d9c3ee216c72025
Pierre Grydbeck <pierre@grydbeck.com>
2012-06-05 18:09:13 +0200

    run only menthor

--- src/main/scala/ch/epfl/em/Gaussian.scala
+++ src/main/scala/ch/epfl/em/Gaussian.scala
@@ -52,7 +52,7 @@ object Gaussian {
       
         for(i <- 1 to numberOfRuns) {
           println("Iteration #" + i + "-----------------------------------------") 
-        
+        /*
           Platform.collectGarbage()
           printStatus("Classic implementation")
           val classic = new GaussianSequential(rc.strategy)(rc.data, rc.k)
@@ -62,7 +62,7 @@ object Gaussian {
           printStatus("Parrallel implementation")
           val parallel = new GaussianParallel(rc.strategy)(rc.data, rc.k)
           parallel.runAlgo()
-        
+        */
           Platform.collectGarbage()
           printStatus("Menthor implementation")
           val menthor = new GaussianMenthor(rc.strategy)(rc.data, rc.k)
